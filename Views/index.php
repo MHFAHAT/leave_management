c3a3f34155c163464658014b3693fa349fec04b3
MHFahat <m.h.fahat20@gmail.com>
2025-09-13 23:16:36 +0600

    login partial

--- Views/index.php
+++ Views/index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="./login.css">
 </head>
 <body>
-    <Div>
-        <form method="POST"> 
+    <Div class="box" >
+        <form method="POST" id="loginbox"> 
              <label for="username">Username</label>
             <input type="text">
+            
         </form>
     </Div>
 </body>
