7be6fead6bcca45c5c5b0fdb470eaf241040703d
MHFahat <m.h.fahat20@gmail.com>
2025-09-24 23:26:35 +0600

    home page edit

--- Views/employee/home.php
+++ Views/employee/home.php
@@ -1,4 +1,40 @@
-<?php 
- echo "<h1> This is employee</h1>";
- 
+<?php
+session_start();
+    if(isset($_SESSION["userId"]))
+    {
+        if(isset($_SESSION["role"])=="emplyee")
+        {
+            
+        }
+
+        else
+        {
+            header("Location:../login.php");
+        }
+
+        
+
+    }
+
+    else
+    {
+        header("Location:../login.php");
+    }
+
+
+    echo "<h1>Welcome Admin. Name: ".$_SESSION["fullName"]."</h1><br>";
+
+    echo "<a href='../logout.php'>logout</a>";
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emplyee Home</title>
+</head>
+<body>
+    
+</body>
+</html>
